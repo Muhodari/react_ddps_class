36e54639f199d663b22c2c613701216e67e5081d
Muhodari Sage <sagemuho@gmail.com>
2021-08-03 15:13:58 +0200

    set handle to true

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const App = ()=> {
+  // lamp one
+const [isLampOneOn,setIsLampOneOn] =useState(false)
+// lamp two
+const [isLampTwoOn,setIsLampTwoOn] =useState(true)
+
+const handleLightSwitchOne=() => setIsLampOneOn(prev =>!prev);
+const handleLightSwitchTwo=() => setIsLampTwoOn(prev =>!prev);  
+
   return (
     <div className="App">
       Start here.
