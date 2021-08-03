ade02ecd34cb98b2f544edb7881c7278d53e8bfb
Muhodari Sage <sagemuho@gmail.com>
2021-08-03 19:07:47 +0200

    added lamp js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 
 // components
 import Lamp from './Lamp';
+
 import LightSwitch from './LightSwitch'
 
 const Room = styled.div`
@@ -11,10 +12,7 @@ width:500px;
 height:500px;
 border:10px solid black;
 margin: 0 auto;
-
-
 `;
-
 
 
 const App = ()=> {
@@ -37,6 +35,7 @@ callback={handleLightSwitchOne}
 switchON={isLampOneOn}
 position= 'left'
 />
+
 <LightSwitch
 name='two'
 callback={handleLightSwitchTwo}
