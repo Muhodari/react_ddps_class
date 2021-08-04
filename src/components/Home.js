9d3b8b19792518c8784879c4779ab4f0f39208ed
Muhodari Sage <sagemuho@gmail.com>
2021-08-04 12:25:14 +0200

    working on fet data to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ import API from '../API'
 // config
 import { POSTER_SIZE,BACKDROP_SIZE,IMAGE_BASE_URL } from "../config";
 // components
-
+import HeroImage from './HeroImage';
 
 // Hooks
 import { useHomeFetch } from './hooks/useHomeFetch';
@@ -16,7 +16,21 @@ const Home =()=>{
 const{state,loading,error} = useHomeFetch();
 
 console.log(state)
-return <div>Home Page</div>
+return (
+<>
+{state.results[0]?(
+    <HeroImage 
+    image={ `${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+    title={state.results[0].orginal_title}
+    text ={state.results[0].overview}
+
+    /> 
+):null
+
+}
+
+</>
+)
 
 }
 
