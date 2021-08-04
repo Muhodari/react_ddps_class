97fece86f21bac9c3f1c59a147b61ff68776180f
Muhodari Sage <sagemuho@gmail.com>
2021-08-04 12:36:17 +0200

    get text and header

--- src/components/Home.js
+++ src/components/Home.js
@@ -18,10 +18,10 @@ const{state,loading,error} = useHomeFetch();
 console.log(state)
 return (
 <>
-{state.results[0]?(
+{ state.results[0]? (
     <HeroImage 
     image={ `${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
-    title={state.results[0].orginal_title}
+    title={state.results[0].original_title}
     text ={state.results[0].overview}
 
     /> 
