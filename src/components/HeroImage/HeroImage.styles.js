0797d5bdceaf920bf038fb130fd6d7147d393283
Muhodari Sage <sagemuho@gmail.com>
2021-08-04 15:00:49 +0200

    fixing errors

--- src/components/HeroImage/HeroImage.styles.js
+++ src/components/HeroImage/HeroImage.styles.js
@@ -2,9 +2,9 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div`
 background: linear-gradient(
-    to bottom, rgba(0,0,0,0,)
-    41%,rgba(0,0,0,0.65)
-    100%,
+    to bottom,
+     rgba(0,0,0,0)41%,
+    rgba(0,0,0,0.65)100%
 ),
 url(${({image}) => image}),var(--darkGrey);
 background: 100%,cover;
--- src/components/Home.js
+++ src/components/Home.js
@@ -23,7 +23,7 @@ return (
     image={ `${IMAGE_BASE_URL} ${BACKDROP_SIZE} ${state.results[0].backdrop_path}`}
     title={state.results[0].original_title}
     text ={state.results[0].overview}
-
+    
     /> 
 ):null
 
