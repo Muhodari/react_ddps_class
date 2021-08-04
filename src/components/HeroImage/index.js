70c86af648cba1dbade5be91ef297af255c0cb31
Muhodari Sage <sagemuho@gmail.com>
2021-08-04 12:13:13 +0200

    added element to horeImage js

--- src/components/HeroImage/index.js
+++ src/components/HeroImage/index.js
@@ -1,5 +1,20 @@
-import styled from "styled-components";
+import React from 'react';
+// styles
+import { Wrapper,Content,Text} from './HeroImage.styles';
 
-export const wrappe = styled.div``;
-export const Content = styled.div``;
-export const Text = styled.div``;
+const HeroImage = ({image,title,text}) => (
+<Wrapper image={image}>
+<Content>
+
+<Text>
+    <h1>{title}</h1>
+    <p>{text}</p>
+</Text>
+
+</Content>
+</Wrapper>
+
+);
+
+
+export default HeroImage;
