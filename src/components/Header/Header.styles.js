a8f727e19d7669b920b496f4e69637166eda06e8
Muhodari Sage <sagemuho@gmail.com>
2021-08-04 10:41:05 +0200

    done with headers

--- src/components/Header/Header.styles.js
+++ src/components/Header/Header.styles.js
@@ -18,8 +18,19 @@ margin: 0 auto;
 `;
 
 export const LogoImg = styled.img`
+
 width: 200px;
+
+@media screen and (max-width:500px){
+    width: 150px;
+}
 
 `;
 
-export const TMDBLogoImg =styled.img``;
+export const TMDBLogoImg =styled.img`
+width: 100px;
+
+@media screen and (max-width:500px){
+    width: 80px;
+}
+`;
