39e7fbd433db283ddcc44db7545f2dac36a9d1ed
Muhodari Sage <sagemuho@gmail.com>
2021-08-04 15:13:46 +0200

    added Text style

--- src/components/HeroImage/HeroImage.styles.js
+++ src/components/HeroImage/HeroImage.styles.js
@@ -36,6 +36,36 @@ margin: 0 auto;
 
 `;
 export const Text = styled.div`
+z-index: 100px;
+max-width: 700px;
+position: absolute;
+bottom: 40px;
+margin-right: 20px;
+min-height: 100px;
+color:var(--white);
+
+h1{
+    font-size: var(--fontSuperBig);
+
+    @media screen and (max-width:720px){
+    font-size: var(--fontBig);
+}
+
+}
+
+p{
+    font-size: var(--fontMed);
+
+    @media screen and (max-width:720px){
+    font-size: var(--fontSmall);
+}
+
+}
+
+@media screen and (max-width:720px){
+width: 100%;
+}
+
 
 
 `;
