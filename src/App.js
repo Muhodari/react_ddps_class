532b2c4352bebb5bbbd49c001be714704ef2c0f2
Muhodari Sage <sagemuho@gmail.com>
2021-08-04 11:33:01 +0200

    added fetch movies from movies database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 //components 
 import Header from './components/Header'; 
+import Home from './components/Home';
 // styles
 import { GlobalStyle } from './GlobalStyle';
 
@@ -12,7 +13,7 @@ const App = ()=> {
 
     <div className="App">
       <Header/>
-      Start here.
+      <Home/>
       <GlobalStyle />
     </div>
   );
