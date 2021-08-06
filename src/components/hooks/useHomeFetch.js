d45942aae858dfb15fb2fab2be945d02a5c00301
Muhodari Sage <sagemuho@gmail.com>
2021-08-06 16:08:51 +0200

    fixing ..

--- src/components/hooks/useHomeFetch.js
+++ src/components/hooks/useHomeFetch.js
@@ -46,6 +46,14 @@ export const useHomeFetch = ()=>{
     fetchMovies(1,searchTerm);
     
     },[searchTerm])
+
+// load more 
+useEffect(()=>{
+if(isLoadingMore) return ;
+
+fetchMovies(state.page + 1 , searchTerm)
+
+},[isLoadingMore,searchTerm, state.page])
     
 return {state,loading,error,searchTerm,setSearchTerm,setIsLoadingMore}
 }
