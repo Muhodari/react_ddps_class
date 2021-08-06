5fc48a9481e61bf2c0fdf2e8e81a37ccb0cc1d0e
Muhodari Sage <sagemuho@gmail.com>
2021-08-06 13:14:34 +0200

    added complete thumbnail

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import { POSTER_SIZE,BACKDROP_SIZE,IMAGE_BASE_URL } from "../config";
 // components
 import HeroImage from './HeroImage';
 import Grid from './Grid';
+import Thumb from './Thumb';
 
 // Hooks
 import { useHomeFetch } from './hooks/useHomeFetch';
@@ -31,7 +32,22 @@ return (
 
 <Grid header='Popular movies'>
     {state.results.map( movie => (
-   <div key={movie.id}> {movie.title} </div>
+//    <div key={movie.id}> {movie.title} </div>,
+
+   <Thumb  
+   key={movie.id}
+   clickable
+   image={
+       movie.poster_path 
+       ? IMAGE_BASE_URL +POSTER_SIZE +movie.poster_path
+       :NoImage
+   }
+    movieId={movie.id}
+    />
+
+
+
+
     ))}
 
 </Grid>
