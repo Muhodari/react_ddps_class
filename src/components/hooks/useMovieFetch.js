18f22079145e73ecce6fb23804140cda0b4c53e6
Muhodari Sage <sagemuho@gmail.com>
2021-08-09 11:58:19 +0200

    re order function

--- src/components/hooks/useMovieFetch.js
+++ src/components/hooks/useMovieFetch.js
@@ -7,32 +7,35 @@ export const useMovieFetch =movieId =>{
     const[loading,setLoading] = useState(true)
     const[error , setError]= useState(false)
 
-    useEffect(()=>{
 // fetch movie function
- const fetchData =async()=>{
+    const fetchMovie =async()=>{
 
-try{
-  setLoading(true)
-  setError(false)
-  const movie = await API.fetchMovie(movieId);
-  const credits= await API.fetchCredits(movieId);
-  
-  //   get directors only   
-   const directors = credits.crew.filter(
-       member => member.job === 'Director'
-   );
+        try{
+          setLoading(true)
+          setError(false)
+          const movie = await API.fetchMovie(movieId);
+          const credits= await API.fetchCredits(movieId);
+          
+          //   get directors only   
+           const directors = credits.crew.filter(
+               member => member.job === 'Director'
+           );
+        
+           setState({
+               ...movie,
+               actors: credits.cast,
+               directors
+           })
+            }
+            catch(error){
+                setError(true)
+            }
+        
+        }
 
-   setState({
-       ...movie,
-       actors: credits.cast,
-       directors
-   })
-    }
-    catch(error){
-        setError(true)
-    }
 
-}
+useEffect(()=>{
+fetchMovie();
     },[movieId]);
 
     return {state,loading,error}
