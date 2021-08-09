15e54f31f3d3a3c13e5ee66a7ddd1fce4ca704b2
Muhodari Sage <sagemuho@gmail.com>
2021-08-09 09:53:32 +0200

    added routes setup

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 //components 
 import Header from './components/Header'; 
 import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
 // styles
 import { GlobalStyle } from './GlobalStyle';
 
@@ -16,6 +18,10 @@ const App=() =>(
 
       <Routes>
 <Route path='/' element={<Home/>} />
+<Route path='/:movieId' element={<Movie/>} />
+<Route path='/*' element={<NotFound/>} />
+
+
       </Routes>
 
       
