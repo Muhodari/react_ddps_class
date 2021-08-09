055cd1c4a209c7f31f7b8063732f6faaa6e25332
Muhodari Sage <sagemuho@gmail.com>
2021-08-09 10:42:17 +0200

    added movie and credits fetct function

--- src/components/hooks/useMovieFetch.js
+++ src/components/hooks/useMovieFetch.js
@@ -6,6 +6,30 @@ export const useMovieFetch =movieId =>{
     const[state,setState] = useState({});
     const[loading,setLoading] = useState(true)
     const[error , setError]= useState(false)
-    
+
+    useEffect(()=>{
+// fetch movie function
+ const fetchData =async()=>{
+
+try{
+  setLoading(true)
+  setError(false)
+  const movie = await API.fetchMovie(movieId);
+  const credits= await API.fetchCredits(movieId);
+  
+
+
+
+
+    }
+    catch(error){
+        setError(true)
+    }
+     
+
+
+}
+
+    },[movieId])
 
 }
