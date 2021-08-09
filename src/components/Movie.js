2b953ae3f013f7b42dfe2f1bab56ad38915223ed
Muhodari Sage <sagemuho@gmail.com>
2021-08-09 10:22:39 +0200

    Movie component ..

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,22 @@
 import react from 'react'
+// config 
+import { IMAGE_BASE_URL, POSTER_SIZE} from '../config';
 
-const Movie = () => <div>Movie</div>;
+// components
+import Grid from './Grid';
+import Spinner from './Spinner';
+// hook
+
+// image
+import NoImage from '../images/no_image.jpg'
+
+const Movie = () => {
+return(
+    <>
+    <div>Movie</div>
+    </>
+)
+
+};
 
 export default Movie
