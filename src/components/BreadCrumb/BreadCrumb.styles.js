a6fec04682a5f3905da0c4bfd130f0af0223a42d
Muhodari Sage <sagemuho@gmail.com>
2021-08-10 08:57:45 +0200

    morning

--- src/components/BreadCrumb/BreadCrumb.styles.js
+++ src/components/BreadCrumb/BreadCrumb.styles.js
@@ -2,4 +2,4 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div``;
 
-export const content = styled.div``;
+export const Content = styled.div``;
--- src/components/BreadCrumb/index.js
+++ src/components/BreadCrumb/index.js
@@ -1,1 +1,3 @@
 import styled from "styled-components";
+// styles
+import { Wrapper,Content   } from "./BreadCrumb.styles";
