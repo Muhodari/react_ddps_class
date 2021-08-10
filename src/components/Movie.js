138ccb3eed88d950e730feb9e4461b5c931ff6e6
Muhodari Sage <sagemuho@gmail.com>
2021-08-10 09:16:02 +0200

    showing component on movie page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,6 +6,7 @@ import { IMAGE_BASE_URL, POSTER_SIZE} from '../config';
 // components
 import Grid from './Grid';
 import Spinner from './Spinner';
+import BreadCrumb from './BreadCrumb';
 // hook
 import { useMovieFetch } from './hooks/useMovieFetch';
 // image
@@ -17,10 +18,15 @@ const Movie = () => {
 
   const {state: movie,loading,error} = useMovieFetch(movieId)
 
-//   console.log(movieId)
+  if(loading) return <Spinner/>;
+  if(error) return <div>something went wrong...</div>;
+
+
 return(
     <>
-    <div>Movie</div>
+    <BreadCrumb movieTitle={movie.original_title} />
+
+    
     </>
 )
 
