937d4e62d6f265ab09a66b616fae00d77e406d0e
Muhodari Sage <sagemuho@gmail.com>
2021-08-11 09:14:56 +0200

    added movie info bar component

--- src/components/MovieInfoBar/index.js
+++ src/components/MovieInfoBar/index.js
@@ -1,3 +1,26 @@
 import react from 'react'
 
+// helpers 
+import { calcTime , convertMoney} from '../../helpers'
+// styles
 import { Wrapper , Content } from './MovieInfoBar.styles'
+
+const MovieInfoBar = ({time , Budget ,revenue }) => (
+<Wrapper>
+    <Content>
+  <div className='column'>
+      <p>running time: {calcTime(time)}</p>
+  </div>
+
+  <div className='column'>
+      <p>Budget: {convertMoney(Budget)}</p>
+  </div>
+  <div className='column'>
+      <p>Revenue: {convertMoney(revenue)}</p>
+  </div>
+    </Content>
+</Wrapper>
+
+)
+
+export default MovieInfoBar
